37db7779fd7218c6f22e2c20704e8c55edd328dc
Georg Braun <georg.braun@inform-software.com>
2018-04-12 17:22:52 +0200

    Start der Implementation eines peristenten Chats

---
+++ PersistentChat/persistentchat.py
@@ -0,0 +1,64 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+import datetime
+
+class ChatMessage(db.Model):
+    user = db.StringProperty(required=True)
+    timestamp = db.DateTimeProperty(auto_now_add=True)
+    message = db.TextProperty(required=True)
+
+    def __str__(self):
+        return "%s (%s): %s" % (self.user, self.timestamp, self.message)
+
+Messages = []
+serviceStartedTimeStamp = 0
+
+class ChatRoomPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.out.write(
+            """<html>
+                <head>
+                    <title>Georg's AppEngine Chat Room</title>
+                </head>
+                <body>
+                    <h1>Welome to Georg's AppEngine Chat Room</h1>
+                    <p> The current time is: %s</p>
+                    <p> The service started at: %s</p>
+            """ % (datetime.datetime.now(), serviceStartedTimeStamp))
+        # Output the set of chat messages
+        global Messages
+        for msg in Messages:
+            self.response.out.write("<p>%s</p>" % msg)
+        self.response.out.write("""
+            <form action="" method="post">
+            <div><b>Name:</b>
+            <textarea name="name" rows="1" cols="20"></textarea></div>
+            <p><b>Message</b></p>
+            <div><textarea name="message" rows="5" cols="60"></textarea></div>
+            <div><input type="submit" value="Send ChatMessage"></input></div>
+            </form>
+            </body>
+            </html>
+        """)
+
+    def post(self):
+        chatter = self.request.get("name")
+        msg = self.request.get("message")
+        global Messages
+        Messages.append(ChatMessage(chatter, msg))
+        # Redirect to the root page to refresh the content
+        self.redirect('/AufgabeEinsVonGeorgBraun')
+
+            
+chatapp = webapp.WSGIApplication([(r'/.*',ChatRoomPage)])
+
+def main():    
+    run_wsgi_app(chatapp)
+
+
+if __name__ == "__main__":
+    global serviceStartedTimeStamp 
+    serviceStartedTimeStamp = datetime.datetime.now()
+    main()
